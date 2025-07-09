4fb6ea015832c54a4da807955feb2c7752644857
milliax <maxchengboy@gmail.com>
2025-07-10 04:42:00 +0800

    add

---
+++ 0.generate_random_inventories/index.ts
@@ -0,0 +1,11 @@
+import { prisma } from "./lib/prisma";
+
+
+(() => {
+    // This script generates random inventories and saves them to the database.
+    // It is intended to be run in a development environment only.
+
+
+
+    
+})()
